8d2ad22749fabc4bdf584de3e21b116bb51e0343
xc2331 <1295750363@qq.com>
2025-03-30 03:02:53 +0800

    平滑相机缩放

--- ThrowHeadPuzzle/Assets/AdjustCamera.cs
+++ ThrowHeadPuzzle/Assets/AdjustCamera.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using System;
 
 public class AdjustCamera : MonoBehaviour
 {
     public CinemachineVirtualCamera virtualCamera;
     public Transform 人物身体;
     public float adjustmentSpeed = 0.1f;
-    public float maxOrthoSize = 10f;
+    public float maxOrthoSize = 20f;
+    public float minOrthoSize = 3f;
+
 
     private Camera mainCamera;
 
@@ -19,16 +22,36 @@ public class AdjustCamera : MonoBehaviour
 
     void Update()
     {
-        if (!IsObjectInCameraView(人物身体))
+        if (!IsObjectOutCameraView(人物身体))
         {
             IncreaseOrthoSize();
         }
+        else if (IsObjectMuchInCameraView(人物身体))
+        {
+            DecreaseOrthoSize();
+        }
     }
 
-    bool IsObjectInCameraView(Transform target)
+    private void DecreaseOrthoSize()
+    {
+        float currentOrthoSize = virtualCamera.m_Lens.OrthographicSize;
+        if (currentOrthoSize > minOrthoSize)
+        {
+            virtualCamera.m_Lens.OrthographicSize -= adjustmentSpeed * Time.deltaTime;
+        }
+    }
+
+    bool IsObjectOutCameraView(Transform target)
     {
         Vector3 viewportPoint = mainCamera.WorldToViewportPoint(target.position);
-        return viewportPoint.x >= 0 && viewportPoint.x <= 1 && viewportPoint.y >= 0 && viewportPoint.y <= 1;
+        Debug.Log(viewportPoint);
+        return viewportPoint.x > 0 && viewportPoint.x < 1 && viewportPoint.y > 0 && viewportPoint.y < 1;
+    }
+
+    bool IsObjectMuchInCameraView(Transform target)
+    {
+        Vector3 viewportPoint = mainCamera.WorldToViewportPoint(target.position);
+        return (viewportPoint.x > 0.2 && viewportPoint.x < 0.4) || (viewportPoint.x > 0.6 && viewportPoint.x < 0.8) || (viewportPoint.y > 0.2 && viewportPoint.y < 0.4) || (viewportPoint.y > 0.6 && viewportPoint.y < 0.8);
     }
 
     void IncreaseOrthoSize()
