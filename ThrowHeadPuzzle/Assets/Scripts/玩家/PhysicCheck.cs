b7f1287765f8cfe082c0bcc97687a43fa2d1f42b
xc2331 <1295750363@qq.com>
2026-04-10 14:25:33 +0800

    修复代码结构

--- ThrowHeadPuzzle/Assets/Scripts/玩家/PhysicCheck.cs
+++ ThrowHeadPuzzle/Assets/Scripts/玩家/PhysicCheck.cs
@@ -1,8 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
-
 
 public class PhysicCheck : MonoBehaviour
 {
@@ -14,10 +13,12 @@ public class PhysicCheck : MonoBehaviour
     public float 检测范围;
     public LayerMask 检测图层;
     private CapsuleCollider2D 胶囊组件;
+
     [Header("状态")]
     public bool isGround;
     public bool 是否撞左墙;
     public bool 是否撞右墙;
+
     private void Awake()
     {
         胶囊组件 = GetComponent<CapsuleCollider2D>();
@@ -28,21 +29,36 @@ public class PhysicCheck : MonoBehaviour
             右墙偏移量 = new Vector2(-胶囊组件.bounds.size.x / 2f + 胶囊组件.offset.x, 0);
         }
     }
+
     public void Update()
     {
         检查();
     }
+
     public void 检查()
     {
         检测偏移量 = new Vector2(-transform.localScale.x * Math.Abs(检测偏移量.x), 检测偏移量.y);
-        isGround = Physics2D.OverlapCircle((Vector2)transform.position + 检测偏移量, 检测范围, 检测图层);
+        isGround = Physics2D.OverlapCircle(
+            (Vector2)transform.position + 检测偏移量,
+            检测范围,
+            检测图层
+        );
         #region 撞左墙判断
-        是否撞左墙 = Physics2D.OverlapCircle((Vector2)transform.position + 左墙偏移量, 检测范围, 检测图层);
+        是否撞左墙 = Physics2D.OverlapCircle(
+            (Vector2)transform.position + 左墙偏移量,
+            检测范围,
+            检测图层
+        );
         #endregion
         #region 撞右墙判断
-        是否撞右墙 = Physics2D.OverlapCircle((Vector2)transform.position + 右墙偏移量, 检测范围, 检测图层);
+        是否撞右墙 = Physics2D.OverlapCircle(
+            (Vector2)transform.position + 右墙偏移量,
+            检测范围,
+            检测图层
+        );
         #endregion
     }
+
     //显示检测题的范围
     private void OnDrawGizmosSelected()
     {
--- ThrowHeadPuzzle/Assets/Scripts/玩家/PlayerController.cs
+++ ThrowHeadPuzzle/Assets/Scripts/玩家/PlayerController.cs
@@ -1,35 +1,57 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq; // 添加LINQ支持
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XInput;
-using System.Linq; // 添加LINQ支持
-
-
 
 public class PlayerController : MonoBehaviour
 {
+    // 重构说明：
+    // 1) 将魔法数字提取为语义化常量。
+    // 2) 将所有 if 控制流条件提取为上一行 bool 变量。
+    // 3) 优先将可安全重命名的英文变量改为中文语义命名。
+    private const int 右键鼠标按钮索引 = 1;
+    private const int 正向朝向值 = 1;
+    private const int 反向朝向值 = -1;
+    private const int 静止朝向值 = 0;
+    private const float 朝向切换速度阈值 = 0.6f;
+    private const float 行走判定速度阈值 = 0.01f;
+    private const float 手柄朝向输入阈值 = 0.03f;
+    private const float 屏幕坐标深度值 = 0f;
+    private const float 缩放轴默认值 = 1f;
+
     #region 公共变量
-    [SerializeField] public InputControl inputControl;//输入
-    private InputAction mousePositionAction;
-    private InputAction mouseAction;
-    private InputAction eKeyAction;
-    [SerializeField] private Vector2 value_inputControl;
-    [SerializeField] private Vector2 aimValue_inputControl;
+    [SerializeField]
+    public InputControl inputControl; //输入
+    private InputAction 鼠标位置输入动作;
+    private InputAction 鼠标左键输入动作;
+    private InputAction E键输入动作;
+
+    [SerializeField]
+    private Vector2 移动输入值;
+
+    [SerializeField]
+    private Vector2 瞄准输入值;
 
     [Header("基本参数")]
     public float 速度;
     public float 跳跃高度;
+
     [Header("物理")]
     public CapsuleCollider2D capsuleCollider;
     public Rigidbody2D rgbody;
     public PhysicCheck physicCheck;
-    [SerializeField] private bool isKeyboard;
-    public int isMoveForward;//表示人物是正走/倒走状态，1代表正走，-1代表倒走
+
+    [SerializeField]
+    private bool 当前为键鼠输入;
+    public int isMoveForward; //表示人物是正走/倒走状态，1代表正走，-1代表倒走
     public RigidbodyController rbController;
-    [SerializeField] Transform ESign;
+
+    [SerializeField]
+    Transform 交互提示标识;
     #endregion
 
     private void Awake()
@@ -37,52 +59,61 @@ public class PlayerController : MonoBehaviour
         inputControl = new InputControl();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
         // 创建一个新的输入动作来获取鼠标位置
-        mousePositionAction = new InputAction(binding: "<Mouse>/position");
-        mouseAction = new InputAction(
+        鼠标位置输入动作 = new InputAction(binding: "<Mouse>/position");
+        鼠标左键输入动作 = new InputAction(
             name: "LeftClick",
             type: InputActionType.Button,
             binding: "<Mouse>/leftButton"
         //interactions: "press(duration=0.1)"  // 添加点击持续阈值
         );
-        eKeyAction = new InputAction("EKeyPress", binding: "<Keyboard>/e");
-        eKeyAction.Enable();
-        mouseAction.Enable();
-        mousePositionAction.Enable();
+        E键输入动作 = new InputAction("EKeyPress", binding: "<Keyboard>/e");
+        E键输入动作.Enable();
+        鼠标左键输入动作.Enable();
+        鼠标位置输入动作.Enable();
         //inputControl.Player.Jump.started += Jump();//旧的方法
         foreach (Transform child in transform)
         {
             // 检查子物体的tag是否为"Head"
-            if (child.CompareTag("Head"))
+            bool 子物体是头部标签 = child.CompareTag("Head");
+            if (子物体是头部标签)
             {
                 // 获取子物体上的RigidbodyController组件
                 rbController = child.GetComponent<RigidbodyController>();
             }
         }
     }
+
     void Update()
     {
         //状态机更新
         UpdateState();
 
         //当人没有头时不改变朝向
-        if (curstate == ThrowState.HeadOnBody || curstate == ThrowState.OtherHead || curstate == ThrowState.ThrowAnimation)
+        bool 当前状态允许根据输入更新朝向 =
+            curstate == ThrowState.HeadOnBody
+            || curstate == ThrowState.OtherHead
+            || curstate == ThrowState.ThrowAnimation;
+        if (当前状态允许根据输入更新朝向)
             CheckAndChangeDirection();
         else
         {
-            int faceDirection;
-            if (rgbody.velocity.x > 0.6)
+            int 人物朝向值;
+            bool 速度超过正向朝向阈值 = rgbody.velocity.x > 朝向切换速度阈值;
+            if (速度超过正向朝向阈值)
             {
-                faceDirection = 1;
-                transform.localScale = new Vector3(faceDirection, 1, 1);
+                人物朝向值 = 正向朝向值;
+                transform.localScale = new Vector3(人物朝向值, 缩放轴默认值, 缩放轴默认值);
             }
 
-            if (rgbody.velocity.x < -0.6)
+            bool 速度低于反向朝向阈值 = rgbody.velocity.x < -朝向切换速度阈值;
+            if (速度低于反向朝向阈值)
             {
-                faceDirection = -1;
-                transform.localScale = new Vector3(faceDirection, 1, 1);
+                人物朝向值 = 反向朝向值;
+                transform.localScale = new Vector3(人物朝向值, 缩放轴默认值, 缩放轴默认值);
             }
         }
     }
+
     void OnEnable()
     {
         InputSystem.onActionChange += OnInputDiviceChange;
@@ -91,21 +122,22 @@ public class PlayerController : MonoBehaviour
 
     private void OnInputDiviceChange(object obj, InputActionChange deviceChange)
     {
-        if (deviceChange == InputActionChange.ActionStarted)
+        bool 输入动作已开始 = deviceChange == InputActionChange.ActionStarted;
+        if (输入动作已开始)
         {
             //Debug.Log(((InputAction)obj).activeControl.device);
-            var cur_device = ((InputAction)obj).activeControl.device;
+            var 当前输入设备 = ((InputAction)obj).activeControl.device;
             //isKeyboard = false;
-            switch (cur_device)
+            switch (当前输入设备)
             {
                 case Keyboard:
-                    isKeyboard = true;
+                    当前为键鼠输入 = true;
                     break;
                 case Mouse:
-                    isKeyboard = true;
+                    当前为键鼠输入 = true;
                     break;
                 case XInputController:
-                    isKeyboard = false;
+                    当前为键鼠输入 = false;
                     break;
             }
         }
@@ -134,9 +166,13 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (canMove)//value_inputControl.magnitude != 0 && 
+        bool 当前允许移动 = canMove;
+        if (当前允许移动) //value_inputControl.magnitude != 0 &&
         {
-            rgbody.velocity = new Vector2(value_inputControl.x * 速度 * Time.deltaTime, rgbody.velocity.y);
+            rgbody.velocity = new Vector2(
+                移动输入值.x * 速度 * Time.deltaTime,
+                rgbody.velocity.y
+            );
         }
     }
 
@@ -144,11 +180,11 @@ public class PlayerController : MonoBehaviour
 
     public enum ThrowState
     {
-        HeadOnBody,//移动 瞄准 默认状态
-        NoHead,//移动
-        OtherHead,//移动 瞄准
-        ThrowAnimation,//该状态结束后应该进入nohead状态
-        PickupAnimation
+        HeadOnBody, //移动 瞄准 默认状态
+        NoHead, //移动
+        OtherHead, //移动 瞄准
+        ThrowAnimation, //该状态结束后应该进入nohead状态
+        PickupAnimation,
     }
 
     [Header("目前状态")]
@@ -177,7 +213,9 @@ public class PlayerController : MonoBehaviour
                 break;
             case ThrowState.PickupAnimation:
                 //把头拼上
-                _currentTrigger.GetComponent<PickUpHead>().OnInteract(new InteractionSignal(gameObject, InteractionType.KeyPress));
+                _currentTrigger
+                    .GetComponent<PickUpHead>()
+                    .OnInteract(new InteractionSignal(gameObject, InteractionType.KeyPress));
                 break;
             case ThrowState.ThrowAnimation:
                 //HeadOnBody=>ThrowAnimation
@@ -205,8 +243,8 @@ public class PlayerController : MonoBehaviour
             case ThrowState.NoHead:
                 //PickUp(最近的头);
                 //捡起头后取消显示提示
-                SpriteRenderer signRenderer = ESign.GetComponent<SpriteRenderer>();
-                signRenderer.enabled = false;  //取消激活 SpriteRenderer
+                SpriteRenderer 提示图标渲染器 = 交互提示标识.GetComponent<SpriteRenderer>();
+                提示图标渲染器.enabled = false; //取消激活 SpriteRenderer
                 break;
             case ThrowState.OtherHead:
                 //
@@ -220,17 +258,17 @@ public class PlayerController : MonoBehaviour
                 HeadAddrgbody();
                 DetachHeads();
                 //抛出
-                Vector2 aim = new Vector2(GetVectorAim().x, 0).normalized;
+                Vector2 瞄准方向 = new Vector2(GetVectorAim().x, 屏幕坐标深度值).normalized;
                 // 获取物体上的PickUpHead组件
-                PickUpHead _picUpHead = rbController.transform.GetComponent<PickUpHead>();
-                _picUpHead.isPickUp = false;
-                rbController.Throw(aim);
+                PickUpHead 头部拾取组件 = rbController.transform.GetComponent<PickUpHead>();
+                头部拾取组件.isPickUp = false;
+                rbController.Throw(瞄准方向);
                 rbController = null;
                 _currentTrigger = null;
                 //停止输入
                 StartCoroutine(StopMoveInAir());
                 //反作用力函数
-                CounterForce(aim);
+                CounterForce(瞄准方向);
                 break;
         }
     }
@@ -244,7 +282,8 @@ public class PlayerController : MonoBehaviour
             case ThrowState.HeadOnBody:
                 //如果鼠标右键，就投掷
                 //if (eKeyAction.triggered)//按下E或者点击 || mouseAction.triggered
-                if (Input.GetMouseButtonDown(1))
+                bool 右键按下准备投掷 = Input.GetMouseButtonDown(右键鼠标按钮索引);
+                if (右键按下准备投掷)
                 {
                     StartState(ThrowState.ThrowAnimation);
                 }
@@ -262,13 +301,15 @@ public class PlayerController : MonoBehaviour
                 //     SpriteRenderer signRenderer = ESign.GetComponent<SpriteRenderer>();
                 //     signRenderer.enabled = false;  //取消激活 SpriteRenderer
                 // }
-                
+
                 //如果按下E，并且范围里有可以捡起的头，就捡起最近的
 
-                if (eKeyAction.triggered)//按下E或者点击 || mouseAction.triggered
+                bool E键被触发 = E键输入动作.triggered;
+                if (E键被触发) //按下E或者点击 || mouseAction.triggered
                 {
                     //满足 检测到可拾取的头 collider触发器
-                    if (CanPickUp())
+                    bool 当前可拾取头部 = CanPickUp();
+                    if (当前可拾取头部)
                     {
                         StartState(ThrowState.PickupAnimation);
                     }
@@ -277,15 +318,19 @@ public class PlayerController : MonoBehaviour
             case ThrowState.OtherHead:
                 //如果按下E，就投掷
                 //if (eKeyAction.triggered)//按下E或者点击 || mouseAction.triggered
-                if (Input.GetMouseButtonDown(1))
+                bool 右键按下投掷其他头部 = Input.GetMouseButtonDown(右键鼠标按钮索引);
+                if (右键按下投掷其他头部)
                 {
                     StartState(ThrowState.ThrowAnimation);
                 }
                 break;
             case ThrowState.PickupAnimation:
                 //结束拾取，检查是否是自己的头
-                if (CheckMyHead()) StartState(ThrowState.HeadOnBody);
-                else StartState(ThrowState.OtherHead);
+                bool 拾取到的是自己的头 = CheckMyHead();
+                if (拾取到的是自己的头)
+                    StartState(ThrowState.HeadOnBody);
+                else
+                    StartState(ThrowState.OtherHead);
                 break;
             case ThrowState.ThrowAnimation:
                 //进入nohead状态
@@ -299,13 +344,14 @@ public class PlayerController : MonoBehaviour
     #region 移动
     public void Jump()
     {
-        if (physicCheck.isGround)
+        bool 当前处于地面 = physicCheck.isGround;
+        if (当前处于地面)
             rgbody.AddForce(transform.up * 跳跃高度, ForceMode2D.Impulse);
     }
 
     public void ValueMove(InputAction.CallbackContext callbackContext)
     {
-        value_inputControl = callbackContext.ReadValue<Vector2>();
+        移动输入值 = callbackContext.ReadValue<Vector2>();
     }
     #endregion
 
@@ -316,7 +362,7 @@ public class PlayerController : MonoBehaviour
     /// <param name="callbackContext"></param>
     public void AimDirection(InputAction.CallbackContext callbackContext)
     {
-        aimValue_inputControl = callbackContext.ReadValue<Vector2>();
+        瞄准输入值 = callbackContext.ReadValue<Vector2>();
     }
 
     /// <summary>
@@ -325,11 +371,14 @@ public class PlayerController : MonoBehaviour
     public int FacetoMouse()
     {
         // 获取鼠标位置
-        Vector2 mouseScreenPos = mousePositionAction.ReadValue<Vector2>();
+        Vector2 鼠标屏幕坐标 = 鼠标位置输入动作.ReadValue<Vector2>();
         // 将鼠标屏幕坐标转换为世界坐标，2D游戏中z轴设为0
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, 0));
-        int direaction = (mouseWorldPos.x - transform.position.x) > 0 ? 1 : -1;
-        return direaction;
+        Vector3 鼠标世界坐标 = Camera.main.ScreenToWorldPoint(
+            new Vector3(鼠标屏幕坐标.x, 鼠标屏幕坐标.y, 屏幕坐标深度值)
+        );
+        bool 鼠标位于角色右侧 = (鼠标世界坐标.x - transform.position.x) > 屏幕坐标深度值;
+        int 人物朝向值 = 鼠标位于角色右侧 ? 正向朝向值 : 反向朝向值;
+        return 人物朝向值;
     }
 
     /// <summary>
@@ -338,18 +387,21 @@ public class PlayerController : MonoBehaviour
     /// <returns></returns>
     public Vector2 GetVectorAim()
     {
-        if (isKeyboard)
+        bool 当前输入设备为键鼠 = 当前为键鼠输入;
+        if (当前输入设备为键鼠)
         {
             // 获取鼠标位置
-            Vector2 mouseScreenPos = mousePositionAction.ReadValue<Vector2>();
+            Vector2 鼠标屏幕坐标 = 鼠标位置输入动作.ReadValue<Vector2>();
             // 将鼠标屏幕坐标转换为世界坐标，2D游戏中z轴设为0
-            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, 0));
-            Vector2 direaction = mouseWorldPos - transform.position;
-            return direaction;
+            Vector3 鼠标世界坐标 = Camera.main.ScreenToWorldPoint(
+                new Vector3(鼠标屏幕坐标.x, 鼠标屏幕坐标.y, 屏幕坐标深度值)
+            );
+            Vector2 瞄准方向向量 = 鼠标世界坐标 - transform.position;
+            return 瞄准方向向量;
         }
         else
         {
-            return aimValue_inputControl;
+            return 瞄准输入值;
         }
     }
 
@@ -358,38 +410,51 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     public void CheckAndChangeDirection()
     {
-        int faceDirection = (int)transform.localScale.x;
-        //Debug.Log(rgbody.velocity.x * faceDirection);
-        if (isKeyboard)//输入不为手柄时
+        int 人物朝向值 = (int)transform.localScale.x;
+        //Debug.Log(rgbody.velocity.x * 人物朝向值);
+        bool 当前输入设备为键鼠 = 当前为键鼠输入;
+        if (当前输入设备为键鼠) //输入不为手柄时
         {
-            faceDirection = FacetoMouse() > 0 ? 1 : -1;
-            transform.localScale = new Vector3(faceDirection, 1, 1);
+            bool 鼠标朝向为右侧 = FacetoMouse() > 屏幕坐标深度值;
+            人物朝向值 = 鼠标朝向为右侧 ? 正向朝向值 : 反向朝向值;
+            transform.localScale = new Vector3(人物朝向值, 缩放轴默认值, 缩放轴默认值);
             //判断正走/倒走
-            if (rgbody.velocity.x * faceDirection > 0.01)//正走
+            bool 正向行走中 = rgbody.velocity.x * 人物朝向值 > 行走判定速度阈值;
+            if (正向行走中) //正走
             {
                 //触发正走的animator
-                isMoveForward = 1;
+                isMoveForward = 正向朝向值;
                 //Debug.Log("正走");
             }
-            else if (rgbody.velocity.x * faceDirection < -0.01)
+            else
             {
-                //触发倒走的animator
-                isMoveForward = -1;
-                //Debug.Log("倒走");
-            }
-            else if (rgbody.velocity.x * faceDirection == 0)
-            {
-                isMoveForward = 0;
-                //Debug.Log("静止");
+                bool 反向行走中 = rgbody.velocity.x * 人物朝向值 < -行走判定速度阈值;
+                if (反向行走中)
+                {
+                    //触发倒走的animator
+                    isMoveForward = 反向朝向值;
+                    //Debug.Log("倒走");
+                }
+                else
+                {
+                    bool 当前静止不动 = rgbody.velocity.x * 人物朝向值 == 屏幕坐标深度值;
+                    if (当前静止不动)
+                    {
+                        isMoveForward = 静止朝向值;
+                        //Debug.Log("静止");
+                    }
+                }
             }
         }
         else
         {
-            if (aimValue_inputControl.x > 0.03)
-                faceDirection = 1;
-            if (aimValue_inputControl.x < -0.03)
-                faceDirection = -1;
-            transform.localScale = new Vector3(faceDirection, 1, 1);
+            bool 手柄输入指向右侧 = 瞄准输入值.x > 手柄朝向输入阈值;
+            if (手柄输入指向右侧)
+                人物朝向值 = 正向朝向值;
+            bool 手柄输入指向左侧 = 瞄准输入值.x < -手柄朝向输入阈值;
+            if (手柄输入指向左侧)
+                人物朝向值 = 反向朝向值;
+            transform.localScale = new Vector3(人物朝向值, 缩放轴默认值, 缩放轴默认值);
         }
     }
     #endregion
@@ -401,10 +466,13 @@ public class PlayerController : MonoBehaviour
     // 直接在原方法中写完整实现（放弃扩展方法）
     public void DetachHeads()
     {
-        var allChildren = GetComponentsInChildren<Transform>();
-        foreach (var child in allChildren)
+        var 所有子物体变换组件 = GetComponentsInChildren<Transform>();
+        foreach (var child in 所有子物体变换组件)
         {
-            if (child != transform && child.CompareTag("Head"))
+            bool 子物体不是自身 = child != transform;
+            bool 子物体是头部标签 = child.CompareTag("Head");
+            bool 子物体可分离为头部 = 子物体不是自身 && 子物体是头部标签;
+            if (子物体可分离为头部)
                 child.SetParent(null);
         }
     }
@@ -418,12 +486,14 @@ public class PlayerController : MonoBehaviour
         foreach (Transform child in transform)
         {
             // 检查子物体的tag是否为"Head"
-            if (child.CompareTag("Head"))
+            bool 子物体是头部标签 = child.CompareTag("Head");
+            if (子物体是头部标签)
             {
                 // 获取子物体上的RigidbodyController组件
                 rbController = child.GetComponent<RigidbodyController>();
 
-                if (rbController != null)
+                bool 找到了刚体控制器 = rbController != null;
+                if (找到了刚体控制器)
                 {
                     // 执行 AddRGbody()
                     rbController.AddRGbody();
@@ -443,7 +513,8 @@ public class PlayerController : MonoBehaviour
         Debug.LogWarning($"没有找到tag为'Head'的子物体");
     }
 
-    [SerializeField] int 反作用力;
+    [SerializeField]
+    int 反作用力;
 
     /// <summary>
     /// 在扔出脑袋时对自己施加反作用力
@@ -451,14 +522,15 @@ public class PlayerController : MonoBehaviour
     /// <exception cref="NotImplementedException"></exception>
     private void CounterForce(Vector2 aim)
     {
-        rgbody.velocity = new Vector2(0, 0);
+        rgbody.velocity = Vector2.zero;
         rgbody.AddForce(-aim.normalized * 反作用力, ForceMode2D.Impulse);
     }
 
     #endregion
 
     #region 捡起头部
-    [SerializeField] public Collider2D _currentTrigger; // 当前所在的触发器
+    [SerializeField]
+    public Collider2D _currentTrigger; // 当前所在的触发器
 
     /// <summary>
     /// 检测周围物体是否能拾取（也就是玩家是否位于"Head"触发器内）
@@ -472,9 +544,12 @@ public class PlayerController : MonoBehaviour
     public bool CheckMyHead()
     {
         // 获取触发器所在物体的 RigidbodyController 组件
-        RigidbodyController controller = _currentTrigger.GetComponentInParent<RigidbodyController>();
+        RigidbodyController 头部控制器 =
+            _currentTrigger.GetComponentInParent<RigidbodyController>();
 
-        return controller != null && controller.isMyHead;
+        bool 找到头部控制器 = 头部控制器 != null;
+        bool 当前控制器是我的头 = 找到头部控制器 && 头部控制器.isMyHead;
+        return 当前控制器是我的头;
     }
 
     // 触发器进入时记录
@@ -486,7 +561,8 @@ public class PlayerController : MonoBehaviour
     // 触发器退出时清空记录
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other == _currentTrigger)
+        bool 退出触发器是当前记录对象 = other == _currentTrigger;
+        if (退出触发器是当前记录对象)
         {
             _currentTrigger = null;
         }
